5bd2ca490235584e01b27223e13391f18c944fb2
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2025-04-27 12:41:20 +0200

    project_koi: Level change works again

---
+++ scripts/level_finish.gd
@@ -0,0 +1,15 @@
+extends Area3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node3D) -> void:
+	var body_groups: Array = body.get_groups()
+
+	if body_groups.get(0) == "Player_Body":
+		print("Level_finished: Loading new level")
+		Level._load_new_level(false, true) # Replace with function body.
